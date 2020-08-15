d5e1e75035f535ca11ff0cb1214fe2e16327d021
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-08-15 18:02:06 +0530

    Fix auth flow

--- lib/main.dart
+++ lib/main.dart
@@ -14,52 +14,58 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext ctx) {
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
       ),
     );
 
-    return Provider(
+    return ChangeNotifierProvider(
       create: (_) => AuthUser(),
-      child: MaterialApp(
-        title: 'InstaChat',
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.black,
-          accentColor: Color.fromRGBO(84, 84, 84, 1),
-          scaffoldBackgroundColor: Colors.black,
-          appBarTheme: AppBarTheme(color: Color.fromRGBO(36, 36, 36, 1)),
-          buttonColor: Color.fromRGBO(36, 36, 36, 1),
-          textTheme: TextTheme(
-            headline6: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
-            ),
-            bodyText1: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-            ),
-            bodyText2: TextStyle(
-              fontSize: 16,
-              height: 1.3,
+      builder: (context, _) {
+        final authUser = Provider.of<AuthUser>(context);
+
+        return MaterialApp(
+          title: 'InstaChat',
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.black,
+            accentColor: Color.fromRGBO(84, 84, 84, 1),
+            scaffoldBackgroundColor: Colors.black,
+            appBarTheme: AppBarTheme(color: Color.fromRGBO(36, 36, 36, 1)),
+            buttonColor: Color.fromRGBO(36, 36, 36, 1),
+            textTheme: TextTheme(
+              headline6: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+              bodyText1: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+              bodyText2: TextStyle(
+                fontSize: 16,
+                height: 1.3,
+              ),
             ),
           ),
-        ),
-        onGenerateRoute: (route) {
-          if (route.name == '/')
-            return MaterialPageRoute(builder: (_) => SplashScreen());
-          else if (route.name == LoginPage.routeName)
-            return MaterialPageRoute(builder: (_) => LoginPage());
-          else if (route.name == ChatsPage.routeName)
-            return MaterialPageRoute(builder: (_) => ChatsPage());
-          else if (route.name == NewChatPage.routeName)
-            return MaterialPageRoute<bool>(builder: (_) => NewChatPage());
-          else
-            return MaterialPageRoute(builder: (_) => SplashScreen());
-        },
-      ),
+          onGenerateRoute: (route) {
+            if (authUser.account == null)
+              return MaterialPageRoute(builder: (_) => SplashScreen());
+            switch (route.name) {
+              case LoginPage.routeName:
+                return MaterialPageRoute(builder: (_) => LoginPage());
+              case ChatsPage.routeName:
+                return MaterialPageRoute(builder: (_) => ChatsPage());
+              case NewChatPage.routeName:
+                return MaterialPageRoute<bool>(builder: (_) => NewChatPage());
+              default:
+                return MaterialPageRoute(builder: (_) => ChatsPage());
+            }
+          },
+        );
+      },
     );
   }
 }
--- lib/models/auth_user.dart
+++ lib/models/auth_user.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class AuthUser {
+class AuthUser extends ChangeNotifier {
   GoogleSignIn _googleSignIn;
   GoogleSignInAccount _account;
 
@@ -12,14 +13,17 @@ class AuthUser {
 
   Future<void> trySignInSilently() async {
     _account = await _googleSignIn.signInSilently();
+    notifyListeners();
   }
 
   Future<void> signIn() async {
     _account = await _googleSignIn.signIn();
+    notifyListeners();
   }
 
   Future<void> signOut() async {
     await _googleSignIn.signOut();
     _account = null;
+    notifyListeners();
   }
 }
