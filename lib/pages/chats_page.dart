c06cd6dbc223d4d3168a1ab1420f64631df50372
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-11-04 17:20:35 +0530

    Create chats service

--- lib/pages/chats_page.dart
+++ lib/pages/chats_page.dart
@@ -1,10 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/auth_user.dart';
-import '../models/chat.dart';
 import '../pages/new_chat_page.dart';
+import '../services/chats_service.dart';
 import '../widgets/chat_tile.dart';
 import '../widgets/insta_app_bar.dart';
 import '../widgets/search_bar.dart';
@@ -16,42 +15,22 @@ class ChatsPage extends StatefulWidget {
 
 class _ChatsPageState extends State<ChatsPage> {
   AuthUser _authUser;
-  List<Chat> chats = [];
+  ChatsService chatsService;
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     _authUser = Provider.of<AuthUser>(context, listen: false);
-    refreshChats();
-  }
-
-  Future<void> refreshChats() async {
-    final userChats = await Firestore.instance
-        .collection('user')
-        .document(_authUser.account.id)
-        .collection('chat')
-        .getDocuments();
-
-    final chatIds = userChats.documents.map((doc) => doc.data['id']).toList();
-
-    // TODO: this could break for more than 10 chatIds
-    if (chatIds.length > 0) {
-      final chatDocs = await Firestore.instance
-          .collection('chat')
-          .where('id', whereIn: chatIds)
-          .getDocuments();
-
-      setState(() {
-        chats = chatDocs.documents.map((d) => Chat.fromMap(d.data)).toList();
-      });
-    }
+    chatsService = ChatsService(_authUser);
+    chatsService.addListener(() => setState(() {}));
+    chatsService.updateChats();
   }
 
   void newChat() async {
     final refresh =
         await Navigator.of(context).pushNamed<bool>(NewChatPage.routeName) ??
             false;
-    if (refresh) refreshChats();
+    if (refresh) chatsService.updateChats();
   }
 
   @override
@@ -68,7 +47,7 @@ class _ChatsPageState extends State<ChatsPage> {
           ],
         ),
         body: RefreshIndicator(
-          onRefresh: refreshChats,
+          onRefresh: chatsService.updateChats,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -84,8 +63,8 @@ class _ChatsPageState extends State<ChatsPage> {
               Expanded(
                 child: ListView.separated(
                   separatorBuilder: (_, __) => SizedBox(height: 8),
-                  itemCount: chats.length,
-                  itemBuilder: (_, i) => ChatTile(chat: chats[i]),
+                  itemCount: chatsService.chats.length,
+                  itemBuilder: (_, i) => ChatTile(chat: chatsService.chats[i]),
                 ),
               ),
             ],
