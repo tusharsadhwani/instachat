1809bd0001d157e256c1349cb4f06143424aff8b
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2020-11-21 19:15:04 +0530

    jwt test

--- server/api/api.go
+++ server/api/api.go
@@ -3,7 +3,22 @@ package api
 import (
 	"os"
 
+	"crypto/rand"
+	"crypto/rsa"
+	"log"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+
+	jwt "github.com/form3tech-oss/jwt-go"
+	jwtware "github.com/gofiber/jwt/v2"
+)
+
+var (
+	// Obviously, this is just a test example. Do not do this in production.
+	// In production, you would have the private key and public key pair generated
+	// in advance. NEVER add a private key to any GitHub repo.
+	privateKey *rsa.PrivateKey
 )
 
 // RunApp runs the server
@@ -34,5 +49,67 @@ func RunApp() {
 	app.Get("/me", GetMe)
 	app.Post("/login/:id", Login)
 
+	// Just as a demo, generate a new private/public key pair on each run. See note above.
+	rng := rand.Reader
+	var err error
+	privateKey, err = rsa.GenerateKey(rng, 2048)
+	if err != nil {
+		log.Fatalf("rsa.GenerateKey: %v", err)
+	}
+
+	// Login route
+	app.Post("/login", login)
+
+	// Unauthenticated route
+	app.Get("/", accessible)
+
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningMethod: "RS256",
+		SigningKey:    privateKey.Public(),
+	}))
+
+	// Restricted Routes
+	app.Get("/restricted", restricted)
+
 	app.Listen(":3000")
+}
+
+func login(c *fiber.Ctx) error {
+	user := c.FormValue("user")
+	pass := c.FormValue("pass")
+
+	// Throws Unauthorized error
+	if user != "john" || pass != "doe" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	// Create token
+	token := jwt.New(jwt.SigningMethodRS256)
+
+	// Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = "John Doe"
+	claims["admin"] = true
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString(privateKey)
+	if err != nil {
+		log.Printf("token.SignedString: %v", err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token": t})
+}
+
+func accessible(c *fiber.Ctx) error {
+	return c.SendString("Accessible")
+}
+
+func restricted(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.SendString("Welcome " + name)
 }
--- server/go.mod
+++ server/go.mod
@@ -3,8 +3,13 @@ module github.com/tusharsadhwani/instachat
 go 1.15
 
 require (
+	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/form3tech-oss/jwt-go v3.2.2+incompatible
 	github.com/gofiber/fiber/v2 v2.2.0
+	github.com/gofiber/jwt/v2 v2.1.0
 	github.com/jinzhu/copier v0.0.0-20201025035756-632e723a6687
+	github.com/klauspost/compress v1.11.3 // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 	gorm.io/driver/postgres v1.0.5
 	gorm.io/gorm v1.20.6
 )
