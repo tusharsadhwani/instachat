00bfbfeb789c4c78c892d04d55947d3493abc1ff
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-01-17 14:47:57 +0530

    Update pre signed url endpoint

--- server/api/image.go
+++ server/api/image.go
@@ -1,27 +1,38 @@
 package api
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofrs/uuid"
 	"github.com/rhnvrm/simples3"
 	"github.com/tusharsadhwani/instachat/config"
 )
 
-//GetPresignedURL generates a pre signed url for S3
-func GetPresignedURL(c *fiber.Ctx) error {
+//GetImagePresignedURL generates a pre signed url for image upload on S3
+func GetImagePresignedURL(c *fiber.Ctx) error {
+	filename := c.Params("filename")
+
 	cfg := config.GetConfig()
-	// fmt.Printf("%#v", cfg)
 
 	s3 := simples3.New(cfg.S3Region, cfg.S3AccessKey, cfg.S3SecretKey)
 
+	timestamp := time.Now()
+	uuid, err := uuid.NewV4()
+	if err != nil {
+		return c.Status(500).SendString("Error in generating uuid")
+	}
+	randomHex := strings.ReplaceAll(uuid.String(), "-", "")
+	objectKey := fmt.Sprintf("%x%s-%s", timestamp.Unix(), randomHex, filename)
+
 	url := s3.GeneratePresignedURL(simples3.PresignedInput{
 		Bucket:        cfg.S3Bucket,
-		ObjectKey:     "test.txt",
+		ObjectKey:     objectKey,
 		Method:        "PUT",
-		Endpoint:      "/",
-		Timestamp:     time.Now(),
-		ExpirySeconds: 86400,
+		Timestamp:     timestamp,
+		ExpirySeconds: 1000,
 	})
 	return c.SendString(url)
 }
