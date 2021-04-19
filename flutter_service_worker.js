ca2053185c244bf3daf2f1d065e0875fa405c875
tusharsadhwani <tusharsadhwani@users.noreply.github.com>
2021-04-19 12:49:59 +0000

    deploy: 9c382c0b7ec6aff598916f95e59bd9a8e0c3f195

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,11 +8,11 @@ const RESOURCES = {
 "manifest.json": "b0aa4e7ee7bf12be57927204f8474ac2",
 "index.html": "0abd324ff4e6308d183db90eee706799",
 "/": "0abd324ff4e6308d183db90eee706799",
-"assets/NOTICES": "f812ac3c57505a99622ccabe9aaaf749",
+"assets/NOTICES": "53e29bbe2ce15ba26206dc08dd15bed3",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/AssetManifest.json": "16b49a9c02d3552cceeefadef0883e97",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
-"main.dart.js": "57cbd4e4db2c0cbf38a0bdf24bc6a327",
+"main.dart.js": "a016ccda7fd62cf985ff2a196fd524f8",
 "version.json": "253fd39db19c4565e977ef29cc14ac5d"
 };
 
